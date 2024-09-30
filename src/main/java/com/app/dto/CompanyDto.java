eaa632cbf2c123425bf7734f76d5d333974f358a
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-09-30 13:10:28 -0400

    4-Create Company Entity & DTO & Enums

--- src/main/java/com/app/dto/CompanyDto.java
+++ src/main/java/com/app/dto/CompanyDto.java
@@ -1,4 +1,19 @@
 package com.app.dto;
 
+import com.app.enums.CompanyStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanyDto {
+
+    private Long id;
+    private String title;
+    private String phone;
+    private String website;
+    private AddressDto address;
+    private CompanyStatus companyStatus;
 }
--- src/main/java/com/app/entity/Company.java
+++ src/main/java/com/app/entity/Company.java
@@ -1,9 +1,26 @@
 package com.app.entity;
 
 import com.app.entity.common.BaseEntity;
+import com.app.enums.CompanyStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "companies")
+@NoArgsConstructor
 public class Company extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String title;
+    private String phone;
+    private String website;
+    @Enumerated(EnumType.STRING)
+    private CompanyStatus companyStatus;
+    @OneToOne
+    private Address address;
 }
