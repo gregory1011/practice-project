55eb0c378829bcdf5e279c7253989796e2dc80c5
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-09-30 13:35:28 -0400

    8-Create Category Entity & DTO & Enums

--- src/main/java/com/app/entity/Category.java
+++ src/main/java/com/app/entity/Category.java
@@ -1,9 +1,24 @@
 package com.app.entity;
 
 import com.app.entity.common.BaseEntity;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+
+@Getter
+@Setter
+@ToString
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category extends BaseEntity {
+
+    private String description;
+    @ManyToOne()
+    @JoinColumn(name = "categories")
+    private Company company;
 }
