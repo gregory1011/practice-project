1c84c5058007ad14426bde4d3f7dbefc0022636b
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-09-30 14:31:06 -0400

    12-findByName method

--- src/main/java/com/app/service/UserService.java
+++ src/main/java/com/app/service/UserService.java
@@ -1,9 +1,9 @@
-package com.app.repository;
+package com.app.service;
 
 import com.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserService {
 
     User findByUsername(String username);
 }
--- src/main/java/com/app/service/impl/UserServiceImpl.java
+++ src/main/java/com/app/service/impl/UserServiceImpl.java
@@ -1,4 +1,18 @@
 package com.app.service.impl;
 
-public class UserServiceImpl {
+import com.app.entity.User;
+import com.app.service.UserService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+
 }
