811b3fde63e4a7207523a822e4084e7eca301be0
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-09-30 13:20:08 -0400

    4-Create Users Entity & DTO & Enums

--- src/main/java/com/app/dto/UserDto.java
+++ src/main/java/com/app/dto/UserDto.java
@@ -1,6 +1,23 @@
 package com.app.dto;
 
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
 
+    private Long id;
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private RoleDto role;
+    private CompanyDto company;
+    private boolean isOnlyAdmin;
 }
--- src/main/java/com/app/entity/User.java
+++ src/main/java/com/app/entity/User.java
@@ -1,9 +1,33 @@
 package com.app.entity;
 
 import com.app.entity.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+
+@Getter
+@Setter
 @Entity
+@Table(name = "users")
+@NoArgsConstructor
 public class User extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private boolean enabled;
+    @ManyToOne()
+    private Role role;
+    @ManyToOne()
+    private Company company;
 }
