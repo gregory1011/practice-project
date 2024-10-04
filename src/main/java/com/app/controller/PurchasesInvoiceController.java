e7c8a778b723e67cbd4075e3850ac4eb4e1cde01
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-10-03 21:27:30 -0400

    invoice

--- src/main/java/com/app/controller/PurchasesInvoiceController.java
+++ src/main/java/com/app/controller/PurchasesInvoiceController.java
@@ -1,12 +1,36 @@
 package com.app.controller;
 
+import com.app.service.InvoiceProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping()
+@RequestMapping("/purchasesInvoices")
 public class PurchasesInvoiceController {
+
+
+    private final InvoiceProductService invoiceProductService;
+
+    public PurchasesInvoiceController( InvoiceProductService invoiceProductService) {
+        this.invoiceProductService = invoiceProductService;
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+
+        model.addAttribute("invoices", invoiceProductService.listAllInvoiceProductsByInvoiceTypePurchase());
+        return "/invoice/purchase-invoice-list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+
+        return "/invoice/purchase-invoice-create";
+    }
+
 
 
 }
