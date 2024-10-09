0fa0ddd89109df98618df497e22778d548e64b69
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-10-09 17:03:11 -0400

    Controllers

--- src/main/java/com/app/controller/PurchasesInvoiceController.java
+++ src/main/java/com/app/controller/PurchasesInvoiceController.java
@@ -1,34 +1,52 @@
 package com.app.controller;
 
+import com.app.dto.InvoiceDto;
+import com.app.enums.ClientVendorType;
+import com.app.enums.InvoiceType;
+import com.app.service.ClientVendorService;
 import com.app.service.InvoiceProductService;
+import com.app.service.InvoiceService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/purchasesInvoices")
+@RequestMapping("/purchaseInvoices")
+@AllArgsConstructor
 public class PurchasesInvoiceController {
 
 
     private final InvoiceProductService invoiceProductService;
+    private final InvoiceService invoiceService;
+    private final ClientVendorService clientVendorService;
 
-    public PurchasesInvoiceController( InvoiceProductService invoiceProductService) {
-        this.invoiceProductService = invoiceProductService;
-    }
 
     @GetMapping("/list")
     public String list(Model model) {
 
-        model.addAttribute("invoices", invoiceProductService.listAllInvoiceProductsByInvoiceTypePurchase());
+        model.addAttribute("invoices", invoiceProductService.listAllInvoiceProductsByInvoiceTypeAndLoggedInUserOrderByInvoiceNoDesc(InvoiceType.PURCHASE));
         return "/invoice/purchase-invoice-list";
     }
 
     @GetMapping("/create")
-    public String create(Model model) {
-
+    public String invoiceCreate(InvoiceDto invoiceDto, Model model) {
+        invoiceDto.setInvoiceNo("P-"+invoiceService.getLastInvoiceId(InvoiceType.PURCHASE));
+        model.addAttribute("newPurchaseInvoice", invoiceDto);
+        model.addAttribute("vendors", clientVendorService.listClientVendorsByClientVendorType(ClientVendorType.VENDOR));
         return "/invoice/purchase-invoice-create";
+    }
+
+
+    @PostMapping("/create")
+    public String invoicePost(@ModelAttribute("invoiceDto") InvoiceDto invoiceDto, Model model) {
+        model.addAttribute("vendors", clientVendorService.listClientVendorsByClientVendorType(ClientVendorType.VENDOR));
+        invoiceService.savePurchaseInvoice(invoiceDto);
+        return "redirect:/purchaseInvoices/create";
     }
 
 
--- src/main/java/com/app/controller/SalesInvoiceController.java
+++ src/main/java/com/app/controller/SalesInvoiceController.java
@@ -1,29 +1,49 @@
 package com.app.controller;
 
+import com.app.dto.InvoiceDto;
+import com.app.enums.ClientVendorType;
 import com.app.enums.InvoiceType;
 import com.app.repository.InvoiceProductRepository;
+import com.app.service.ClientVendorService;
 import com.app.service.InvoiceProductService;
+import com.app.service.InvoiceService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+@AllArgsConstructor
 @Controller
 @RequestMapping("/salesInvoices")
 public class SalesInvoiceController {
 
     private final InvoiceProductService invoiceProductService;
-
-    public SalesInvoiceController(InvoiceProductService invoiceProductService) {
-        this.invoiceProductService = invoiceProductService;
-    }
+    private final ClientVendorService clientVendorService;
+    private final InvoiceService invoiceService;
 
     @GetMapping("/list")
     public String list(Model model) {
-
-//        model.addAttribute("invoices", invoiceProductService.listAllInvoiceProductsByInvoiceType(InvoiceType.SALES));
+        model.addAttribute("invoices", invoiceProductService.listAllInvoiceProductsByInvoiceTypeAndLoggedInUserOrderByInvoiceNoDesc(InvoiceType.SALES));
         return "/invoice/sales-invoice-list";
+    }
+
+    @GetMapping("/create")
+    public String createSaleInvoice(Model model) {
+
+        model.addAttribute("newSalesInvoice", new InvoiceDto());
+        model.addAttribute("clients", clientVendorService.listAllClientVendors());
+        return "/invoice/sales-invoice-create";
+    }
+
+    @PostMapping("/create")
+    public String postSaleInvoice(@ModelAttribute("invoiceDto") InvoiceDto invoiceDto, Model model) {
+        model.addAttribute("newSalesInvoice", invoiceDto);
+        invoiceService.saveSaleInvoice(invoiceDto);
+        return "/invoice/sales-invoice-create";
     }
 
 }
