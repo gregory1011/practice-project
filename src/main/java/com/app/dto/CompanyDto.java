c3e2960abbd28b6bee69f0b802a306b38d81162f
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-04 11:47:50 -0500

    Company

--- src/main/java/com/app/dto/CompanyDto.java
+++ src/main/java/com/app/dto/CompanyDto.java
@@ -3,6 +3,11 @@ package com.app.dto;
 import com.app.enums.CompanyStatus;
 import lombok.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @ToString
@@ -11,9 +16,23 @@ import lombok.*;
 public class CompanyDto {
 
     private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 2, max = 100, message = "Title should be 2-100 characters long.")
     private String title;
+
+    @NotBlank(message = "Phone Number is a required field.")
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$" // +111 (202) 555-0125  +1 (202) 555-0125
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"                                  // +111 123 456 789
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$", message = "Phone Number is required field and may be in any valid phone number format.")
     private String phone;
+
+    @NotBlank(message = "Website is required field.")
+    @Pattern(regexp = "^(https?://)?([a-zA-Z0-9.-]+)\\.([a-zA-Z]{2,})(:[0-9]+)?(/[^/]+)*$")
     private String website;
+
+    @Valid
     private AddressDto address;
+
     private CompanyStatus companyStatus;
 }
--- src/main/java/com/app/service/CompanyService.java
+++ src/main/java/com/app/service/CompanyService.java
@@ -9,4 +9,8 @@ public interface CompanyService {
     CompanyDto getCompanyById(Long id);
     Long getCompanyIdByLoggedInUser(String companyId);
     List<CompanyDto> listAllCompanies();
+    void saveCompany(CompanyDto newCompany);
+    void activateCompany(Long id);
+    void deactivateCompany(Long id);
+    void updateCompany(Long id, CompanyDto companyDto);
 }
