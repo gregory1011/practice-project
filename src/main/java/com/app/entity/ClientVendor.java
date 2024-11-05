9425540a36148a1835fb7c56d79c48bf47473bd9
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-05 15:23:45 -0500

    ClientVendor

--- src/main/java/com/app/entity/ClientVendor.java
+++ src/main/java/com/app/entity/ClientVendor.java
@@ -2,20 +2,29 @@ package com.app.entity;
 
 import com.app.entity.common.BaseEntity;
 import com.app.enums.ClientVendorType;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
-
+@Getter
+@Setter
 @Entity
 @Table(name = "clients_vendors")
 public class ClientVendor extends BaseEntity {
 
     private String clientVendorName;
+
     private String phone;
+
     private String website;
+
     @Enumerated(EnumType.STRING)
     private ClientVendorType clientVendorType;
-    @OneToOne(fetch = FetchType.LAZY)
+
+    @OneToOne(cascade = CascadeType.PERSIST)
     private Address address;
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne()
     private Company company;
 }
