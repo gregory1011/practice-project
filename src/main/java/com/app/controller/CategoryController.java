eb498c4f165f6350a4f01ea8ed0742cf51be318a
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-06 08:32:56 -0500

    Category

--- src/main/java/com/app/controller/CategoryController.java
+++ src/main/java/com/app/controller/CategoryController.java
@@ -1,24 +1,70 @@
 package com.app.controller;
 
-
+import com.app.dto.CategoryDto;
+import com.app.entity.Category;
+import com.app.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 
 @Controller
-@RequestMapping("/category")
+@RequestMapping("/categories")
 @AllArgsConstructor
 public class CategoryController {
 
 
+    private final CategoryService categoryService;
+
     @GetMapping("/list")
     public String listAllCategory(Model model) {
-
-
+        model.addAttribute("categories", categoryService.listAllCategories());
         return "category/category-list";
     }
 
+    @GetMapping("/create")
+    public String createCategory(Model model) {
+        model.addAttribute("newCategory", new Category());
+        return "category/category-create";
+    }
+
+    @PostMapping("/create")
+    public String createCategory(@Valid @ModelAttribute("newCategory") CategoryDto newCategory, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "category/category-create";
+        }
+        categoryService.saveCategory(newCategory);
+        return "redirect:/categories/list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateCategory( @PathVariable("id") Long id, Model model) {
+        model.addAttribute("category", categoryService.getCategoryById(id));
+        return "category/category-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateCategory(@Valid @PathVariable("id") Long id, @ModelAttribute("category")CategoryDto category, BindingResult bindingResult, Model model) {
+        boolean descriptionExists = categoryService.isDescriptionExists(category.getDescription());
+        if (descriptionExists) {
+            bindingResult.rejectValue("description"," ", "This category description already exists");
+        }
+        if (bindingResult.hasErrors()) {
+           model.addAttribute("category", categoryService.getCategoryById(id));
+           return "category/category-update";
+       }
+       categoryService.updateCategory(id, category);
+       return "redirect:/categories/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable("id") Long id) {
+        categoryService.deleteById(id);
+        return "redirect:/categories/list";
+    }
 
 }
