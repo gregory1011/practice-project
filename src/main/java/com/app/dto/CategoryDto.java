c149d880f45c1b552dff800743fb844f940f6af9
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-06 08:33:02 -0500

    Category

--- src/main/java/com/app/dto/CategoryDto.java
+++ src/main/java/com/app/dto/CategoryDto.java
@@ -3,6 +3,9 @@ package com.app.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
@@ -12,7 +15,13 @@ import lombok.*;
 public class CategoryDto {
 
     private Long id;
+
+    @NotBlank(message = "Description is a required field.")
+    @Size(min = 2, max = 100, message = "Description must be between 2 and 100 characters long.")
     private String description;
+
     private CompanyDto company;
+
     private boolean hasProduct;
+
 }
