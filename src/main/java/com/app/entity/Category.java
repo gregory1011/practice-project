84d4295400401748826c5fd35d2b654774b8115b
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-06 08:32:49 -0500

    Category

--- src/main/java/com/app/entity/Category.java
+++ src/main/java/com/app/entity/Category.java
@@ -2,14 +2,27 @@ package com.app.entity;
 
 import com.app.entity.common.BaseEntity;
 import lombok.*;
+import org.hibernate.annotations.Where;
+
 import javax.persistence.*;
+import javax.validation.Constraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
 
-
-@Entity
-@Table(name = "categories")
+@Getter
+@Setter
+@Entity(name = "categories")
+@Where(clause = "is_deleted=false")
 public class Category extends BaseEntity {
 
+
     private String description;
+
     @ManyToOne()
     private Company company;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> product;
 }
