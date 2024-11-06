f5123cd7291f6905eb03326df6aaf4f215e828ae
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-06 08:33:26 -0500

    Category

---
+++ src/main/java/com/app/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,76 @@
+package com.app.service.impl;
+
+import com.app.dto.CategoryDto;
+import com.app.dto.CompanyDto;
+import com.app.entity.Category;
+import com.app.repository.CategoryRepository;
+import com.app.service.CategoryService;
+import com.app.service.SecurityService;
+import com.app.util.MapperUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+    private final SecurityService securityService;
+
+    @Override
+    public List<CategoryDto> listAllCategories() {
+        Long id = securityService.getLoggedInUser().getCompany().getId();
+        List<Category> list = categoryRepository.findAll();
+        return list.stream()
+                .filter(category -> category.getCompany().getId().equals(id))
+                .sorted(Comparator.comparing(Category::getDescription))
+                .map(each-> {  // block of code t determine if it hasProducts true or false;
+                    CategoryDto dto = mapperUtil.convert(each, new CategoryDto());
+                    dto.setHasProduct(!each.getProduct().isEmpty());
+                    return dto;
+                })
+                .toList();
+    }
+
+    @Override
+    public void saveCategory(CategoryDto dto) {
+        CompanyDto company = securityService.getLoggedInUser().getCompany();
+        dto.setCompany(company);
+        categoryRepository.save(mapperUtil.convert(dto, new Category()));
+    }
+
+    @Override
+    public CategoryDto getCategoryById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(RuntimeException::new);
+        CategoryDto dto = mapperUtil.convert(category, new CategoryDto());
+        dto.setHasProduct(!category.getProduct().isEmpty());
+        return dto;
+    }
+
+    @Override
+    public void updateCategory(Long id, CategoryDto dto) {
+        dto.setId(id);
+        Category category = categoryRepository.findById(id).orElseThrow(RuntimeException::new);
+        category.setDescription(dto.getDescription());
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public boolean isDescriptionExists(String description) {
+        return categoryRepository.existsAllByDescriptionAndCompanyId(description, securityService.getLoggedInUser().getCompany().getId());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(RuntimeException::new);
+        category.setIsDeleted(true);
+        category.setDescription(category.getDescription() + "-"+category.getId());
+        categoryRepository.save(category);
+    }
+
+
+}
