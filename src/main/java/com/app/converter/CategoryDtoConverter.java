50c0efec237ead3d55966bc80f7975cc920a942f
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-06 08:33:09 -0500

    Category

---
+++ src/main/java/com/app/converter/CategoryDtoConverter.java
@@ -0,0 +1,21 @@
+package com.app.converter;
+
+import com.app.dto.CategoryDto;
+import com.app.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class CategoryDtoConverter implements Converter<String, CategoryDto> {
+
+    private final CategoryService categoryService;
+
+    @Override
+    public CategoryDto convert(String source) {
+        if (source.isEmpty()) return null;
+        return categoryService.getCategoryById(Long.parseLong(source));
+    }
+
+}
