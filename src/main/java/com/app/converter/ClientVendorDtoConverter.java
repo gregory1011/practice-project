4c702ffa5e0cd2e09568c0acb2ce4bb032e5985a
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-07 15:56:59 -0500

    ClientVendor

---
+++ src/main/java/com/app/converter/ClientVendorDtoConverter.java
@@ -0,0 +1,21 @@
+package com.app.converter;
+
+import com.app.dto.ClientVendorDto;
+import com.app.service.ClientVendorService;
+import lombok.AllArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@AllArgsConstructor
+public class ClientVendorDtoConverter implements Converter<String, ClientVendorDto> {
+
+    private final ClientVendorService clientVendorService;
+
+    @Override
+    public ClientVendorDto convert(String source) {
+        if (source.isEmpty()) return null;
+        return clientVendorService.getClientVendorById(Long.parseLong(source));
+    }
+}
