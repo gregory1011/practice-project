0e59c30d44b2078e9dddc4c48c59ab813e0b4d10
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-08 12:09:39 -0500

    dto

--- src/main/java/com/app/dto/ProductDto.java
+++ src/main/java/com/app/dto/ProductDto.java
@@ -4,6 +4,10 @@ package com.app.dto;
 import com.app.enums.ProductUnit;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 
 @Getter
 @Setter
@@ -13,10 +17,21 @@ import lombok.*;
 public class ProductDto {
 
     private Long id;
+
+    @NotBlank(message = "Product name is a required field.")
     private String name;
+
     private Integer quantityInStock;
+
+    @Positive
     private Integer lowLimitAlert;
+
+    @NotNull(message = "Product unit is a required field.")
     private ProductUnit productUnit;
+
+    @NotNull(message = "Category is a required field.")
     private CategoryDto category;
-    private boolean hasProduct;
+
+    private boolean hasInvoiceProduct;
+
 }
