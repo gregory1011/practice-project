85e00a2562b7fc8d9819dfff6d74933c5a84f352
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-08 12:11:03 -0500

    Enums

--- src/main/java/com/app/dto/InvoiceProductDto.java
+++ src/main/java/com/app/dto/InvoiceProductDto.java
@@ -2,6 +2,7 @@ package com.app.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 
@@ -13,11 +14,28 @@ import java.math.BigDecimal;
 public class InvoiceProductDto {
 
     private Long id;
+
+    @NotNull(message = "Quantity is a required field")
+    @Max(value = 100, message = "Quantity should not be greater than 100")
+    @Positive
     private Integer quantity;
+
+    @NotNull(message = "Price is a required field.")
+    @Positive
     private BigDecimal price;
+
+    @NotNull(message = "Tax is a required field.")
+    @Min(value = 0, message = "Tax should be at least 0 %")
+    @Max(value = 20, message = " Tax should not be greater than 20 %")
     private Integer tax;
+
     private BigDecimal total;
+
     private BigDecimal profitLoss;
+
     private Integer remainingQuantity;
+
+    @NotNull(message = "Product is required field")
     private ProductDto product;
+
 }
