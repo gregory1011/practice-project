95a6b9edb10f94c5386ac8bbdc560232e9ecdcc2
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-09 13:46:30 -0500

    AppProject

--- src/main/java/com/app/PracticeProjectApplication.java
+++ src/main/java/com/app/PracticeProjectApplication.java
@@ -3,15 +3,16 @@ package com.app;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients // for consuming 3rd party api with FeignClients
 public class PracticeProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeProjectApplication.class, args);
     }
-
 
     @Bean
     ModelMapper modelMapper() {
