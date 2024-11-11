bb3cc42ea47321162349ab1ec5dad6ff5ee1c537
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-11 11:15:14 -0500

    Exception classes

--- src/main/java/com/app/exceptions/UserNotFoundException.java
+++ src/main/java/com/app/exceptions/UserNotFoundException.java
@@ -1,7 +1,12 @@
 package com.app.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
+
     public UserNotFoundException(String message) {
         super(message);
     }
+    public UserNotFoundException() {
+        super("User not found");
+    }
+
 }
