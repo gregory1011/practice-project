c31b9b81018b0f26bec697999b603ecbd3e760c6
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-12 09:06:48 -0500

    Dto

---
+++ src/main/java/com/app/dto/common/CountryDto.java
@@ -0,0 +1,26 @@
+package com.app.dto.common;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.annotation.Generated;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Generated("jsonschema2pojo")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CountryDto {
+
+    @JsonProperty("country_name")
+    String countryName;
+}
