5e3ee5e5737dac1af64badb948f93ac1d607f66b
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-13 09:01:14 -0500

    Months

---
+++ src/main/java/com/app/enums/Months.java
@@ -0,0 +1,21 @@
+package com.app.enums;
+
+
+import lombok.Getter;
+
+@Getter
+public enum Months {
+
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+}
