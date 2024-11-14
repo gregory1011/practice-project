5a6742b5bcf66c2d81c73a3b02bb69c175aad58c
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-14 10:12:18 -0500

    AddressDto

--- src/main/java/com/app/dto/AddressDto.java
+++ src/main/java/com/app/dto/AddressDto.java
@@ -17,23 +17,22 @@ public class AddressDto {
 
     private Long id;
     @NotBlank(message = "Address is a required field.")
-    @Size(min= 2, max = 100, message = "Address should have 2-100 characters long.")
+    @Size(min= 2, max = 50, message = "Address should have 2-50 characters long.")
     private String addressLine1;
 
     @NotBlank(message = "Address is a required field.")
-    @Size(min= 2, max = 100, message = "Address should have 2-100 characters long.")
+    @Size(min= 2, max = 50, message = "Address should have 2-50 characters long.")
     private String addressLine2;
 
     @NotBlank(message = "City is a required field.")
-    @Size(min= 2, max = 50, message = "City should have 2-50 characters long.")
+    @Size(min= 2, max = 25, message = "City should have 2-25 characters long.")
     private String city;
 
     @NotBlank(message = "State is a required field.")
-    @Size(min= 2, max = 50, message = "State should have 2-50 characters long.")
+    @Size(min= 2, max = 25, message = "State should have 2-50 characters long.")
     private String state;
 
     @NotBlank(message = "Country is a required field.")
-    @Size(min= 2, max = 50, message = "Country should have 2-50 characters long.")
     private String country;
 
     @NotBlank(message = "Zip Code is a required field.")
