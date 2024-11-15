3b5005374ed2ed8da35f60a1230f6b6aa578b1e1
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-15 08:59:18 -0500

    InvoiceType

--- src/main/java/com/app/enums/InvoiceType.java
+++ src/main/java/com/app/enums/InvoiceType.java
@@ -1,17 +1,15 @@
 package com.app.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum InvoiceType {
 
-    PURCHASE("Purchase"), SALES("Sales");
+    PURCHASE("Purchase"),
+    SALES("Sales");
 
     private final String value;
-
-    private InvoiceType(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
 
 }
