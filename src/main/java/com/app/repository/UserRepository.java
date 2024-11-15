97f1f49b0dbf4ec2ba0d76ce14dd0eb1e73e27e9
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-15 09:00:15 -0500

    UserRepository

--- src/main/java/com/app/repository/UserRepository.java
+++ src/main/java/com/app/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.app.repository;
 
+import com.app.dto.UserDto;
 import com.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -11,4 +14,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+    Integer countAllByCompany_IdAndRole_Description(Long id, String description);
+    List<User> findAllByRole_id(Long i);
+    List<User> findAllByCompany_id(Long l);
 }
