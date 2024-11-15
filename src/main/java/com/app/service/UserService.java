f08d7a75d32484781be05521f02609024603f171
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-15 09:00:03 -0500

    UserService

--- src/main/java/com/app/service/UserService.java
+++ src/main/java/com/app/service/UserService.java
@@ -13,4 +13,5 @@ public interface UserService {
     void saveUser(UserDto userDto);
     void updateUser(Long id, UserDto user);
     void deleteUser(Long id);
+    boolean isUsernameExists(UserDto userDto);
 }
