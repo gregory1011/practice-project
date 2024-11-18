e1636221a17e24594f3aa91da62c54e6bfc845ff
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-18 10:37:38 -0500

    UserRepository

--- src/main/java/com/app/repository/UserRepository.java
+++ src/main/java/com/app/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.app.repository;
 
-import com.app.dto.UserDto;
 import com.app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +14,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
-    Integer countAllByCompany_IdAndRole_Description(Long id, String description);
+    @Query(value = "select count(*) from users  where company_id= ?1 and  role_id= 2", nativeQuery = true)
+    Integer countAllByCompany_IdAndRoleIsAdmin(Long id); // role_id=2 for Admin
     List<User> findAllByRole_id(Long i);
     List<User> findAllByCompany_id(Long l);
 }
