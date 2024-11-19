1e42dd9e9ea11a356921873f364b81764e643402
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-19 15:28:32 -0500

    AddressServiceImpl

--- src/main/java/com/app/service/impl/AddressServiceImpl.java
+++ src/main/java/com/app/service/impl/AddressServiceImpl.java
@@ -5,16 +5,17 @@ import com.app.client.CountryClient;
 import com.app.dto.common.CountryDto;
 import com.app.dto.common.TokenDto;
 import com.app.service.AddressService;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Objects;
 
 
-@Log4j2
+@Slf4j
 @Service
 public class AddressServiceImpl implements AddressService {
 
@@ -37,44 +38,32 @@ public class AddressServiceImpl implements AddressService {
     }
 
 
-//    @Scheduled(fixedRate = 100_000) // execute every 100 sec
-//    public void scheduleCountryList(){
-//        fetchCountryListAsync();
-//    }
+    @Scheduled(fixedRate = 100_000) // execute every 100 sec
+    public void scheduleCountryList(){
+        fetchCountryListAsync();
+    }
 
-//    @Async  // runs this method asynchronously (with another thread)
-//    public CompletableFuture<List<String>> fetchCountryListAsync() {
-//        log.info("... fetching Countries with thread: {}", Thread.currentThread().getName());
-//
-//        ResponseEntity<List<CountryDto>> response= countryClient.getCountry(getBearerToken());
-//        if (response.getStatusCode().is2xxSuccessful()) {
-//            List<String> list = Objects.requireNonNull(response.getBody())
-//                    .stream().map(CountryDto::getCountryName)
-//                    .toList();
-//            displayCountryList = list;
-//
-//        } else if (displayCountryList.isEmpty()) {
-//            log.info("... could not fetched Countries, assigning back-up list");
-//            displayCountryList= List.of("United States", "Canada", "France", "India", "Japan", "United Kingdom");
-//        }
-//        return CompletableFuture.completedFuture(displayCountryList);
-//    }
+    @Async  // runs this method asynchronously (with another thread)
+    public List<String> fetchCountryListAsync() {
+        log.info("... fetching Countries with thread: {}", Thread.currentThread().getName());
+
+        ResponseEntity<List<CountryDto>> response= countryClient.getCountry(getBearerToken());
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            displayCountryList = Objects.requireNonNull(response.getBody())
+                    .stream().map(CountryDto::getCountryName).toList();
+        } else if (displayCountryList.isEmpty()) {
+            log.info("... could not fetched Countries, assigning back-up list");
+            displayCountryList= List.of("United States", "Canada", "France", "India", "Japan", "United Kingdom");
+        }
+
+        return displayCountryList;
+    }
 
     @ExecutionTime
     @Override
     public List<String> listAllCountries() {
-        log.info("... fetching Countries with thread: {}", Thread.currentThread().getName());
-        ResponseEntity<List<CountryDto>> response = countryClient.getCountry(getBearerToken());
-
-        if (response.getStatusCode().is2xxSuccessful()) {
-            displayCountryList = Objects.requireNonNull(response.getBody()).stream()
-                    .map(CountryDto::getCountryName)
-                    .toList();
-        } else if (displayCountryList.isEmpty()) {
-            log.info("... could not fetched Countries, assigning back-up list");
-            return displayCountryList = List.of("United Kingdom", "Canada", "France", "India");
-        }
-        return displayCountryList;
+        return fetchCountryListAsync();
     }
 
 
