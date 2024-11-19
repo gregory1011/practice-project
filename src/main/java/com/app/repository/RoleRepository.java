d861ddc17cd6447bf5747486b6aacf15227f7da7
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-19 15:29:06 -0500

    RoleRepository

--- src/main/java/com/app/repository/RoleRepository.java
+++ src/main/java/com/app/repository/RoleRepository.java
@@ -4,6 +4,7 @@ import com.app.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
