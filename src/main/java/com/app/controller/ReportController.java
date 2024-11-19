24b5c31d3c33ffc992ef3f709401d8998949c6e9
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-19 15:30:18 -0500

    ReportController

--- src/main/java/com/app/controller/ReportController.java
+++ src/main/java/com/app/controller/ReportController.java
@@ -1,11 +1,12 @@
 package com.app.controller;
 
-
+import com.app.service.ReportService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/reports")
@@ -13,15 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReportController {
 
 
+    private final ReportService reportService;
+
+
     @GetMapping("/profitLossData")
     public String listProfitLossData(Model model) {
-
-        return "/report/profit-loss-report";
+        model.addAttribute("monthlyProfitLossDataMap", reportService.listMonthlyProfitLossMap());
+        return "report/profit-loss-report";
     }
 
     @GetMapping("/stockData")
     public String listStockData(Model model) {
-
-        return "/report/stock-report";
+        model.addAttribute("invoiceProducts", reportService.listAllInvoiceProductsOfCompany());
+        return "report/stock-report";
     }
+
 }
