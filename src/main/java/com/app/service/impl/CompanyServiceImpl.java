4a89c8caae360deb5d051cc9dee00e691694c1cc
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-20 12:08:05 -0500

    CompanyServiceImpl

--- src/main/java/com/app/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/app/service/impl/CompanyServiceImpl.java
@@ -7,6 +7,7 @@ import com.app.entity.Address;
 import com.app.entity.Company;
 import com.app.entity.User;
 import com.app.enums.CompanyStatus;
+import com.app.exceptions.CompanyNotFoundException;
 import com.app.repository.CompanyRepository;
 import com.app.repository.UserRepository;
 import com.app.service.CompanyService;
@@ -27,24 +28,18 @@ public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyRepository companyRepository;
     private final MapperUtil mapperUtil;
-    private final UserRepository userRepository;
     private final SecurityService securityService;
 
 
     @Override
-    public CompanyDto getCompanyById(Long id) {
-        Company company = companyRepository.findById(id).orElseThrow(() -> new RuntimeException("Company not found"));
+    public CompanyDto listCompanyById(Long id) {
+        Company company = companyRepository.findById(id).orElseThrow(() -> new CompanyNotFoundException("Company not found"));
         return mapperUtil.convert(company, new CompanyDto());
     }
 
     @Override
-    public Long getCompanyIdByLoggedInUser(String companyId) {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username;
-        if (principal instanceof UserDetails) username = ((UserDetails)principal).getUsername();
-        else username = principal.toString();
-        User loggedUser = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return loggedUser.getCompany().getId();
+    public CompanyDto getCompanyByLoggedInUser() {
+        return securityService.getLoggedInUser().getCompany();
     }
 
     @Override
@@ -53,7 +48,7 @@ public class CompanyServiceImpl implements CompanyService {
         UserDto user = securityService.getLoggedInUser();
         List<CompanyDto> companies= new ArrayList<>();
         if (user.getRole().getDescription().equals("Root User")){
-           companies= list.stream().filter(each -> !each.getTitle().equals(user.getCompany().getTitle())).map(each -> mapperUtil.convert(each, new CompanyDto())).toList();
+            companies= list.stream().filter(each -> !each.getTitle().equals(user.getCompany().getTitle())).map(each -> mapperUtil.convert(each, new CompanyDto())).toList();
         }else if(user.getRole().getDescription().equals("Admin")){
             companies= list.stream().filter(each -> each.getTitle().equals(user.getCompany().getTitle())).map(each -> mapperUtil.convert(each, new CompanyDto())).toList();
         }
@@ -62,10 +57,6 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public void saveCompany(CompanyDto dto) {
-//        List<String> titles = companyRepository.findAll().stream().map(Company::getTitle).toList();
-//        if(titles.contains(dto.getTitle())){
-//            throw new RuntimeException("Title already exists");
-//        }
         dto.setCompanyStatus(CompanyStatus.PASSIVE);
         Company company = mapperUtil.convert(dto, new Company());
         companyRepository.save(company);
@@ -73,26 +64,33 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public void activateCompany(Long id) {
-        Company company = companyRepository.findById(id).orElseThrow(() -> new RuntimeException("Company not found"));
+        Company company = companyRepository.findById(id).orElseThrow(CompanyNotFoundException::new);
         company.setCompanyStatus(CompanyStatus.ACTIVE);
         companyRepository.save(company);
     }
 
     @Override
     public void deactivateCompany(Long id) {
-        Company company = companyRepository.findById(id).orElseThrow(() -> new RuntimeException("Company not found"));
+        Company company = companyRepository.findById(id).orElseThrow(CompanyNotFoundException::new);
         company.setCompanyStatus(CompanyStatus.PASSIVE);
         companyRepository.save(company);
     }
 
     @Override
     public void updateCompany(Long id, CompanyDto companyDto) {
-        Company company = companyRepository.findById(id).orElseThrow(() -> new RuntimeException("Company not found"));
+        Company company = companyRepository.findById(id).orElseThrow(CompanyNotFoundException::new);
         company.setPhone(companyDto.getPhone());
         company.setTitle(companyDto.getTitle());
         company.setWebsite(companyDto.getWebsite());
         company.setAddress(mapperUtil.convert(companyDto.getAddress(), new Address()));
         companyRepository.save(company);
+    }
+
+    @Override
+    public boolean titleExist(CompanyDto dto) {
+        Company company = companyRepository.findByTitle(dto.getTitle());
+        if (company == null)return false;
+        return company.getTitle().equals(dto.getTitle());
     }
 
 
