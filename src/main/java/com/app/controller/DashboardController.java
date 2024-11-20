b8fa2a44bdd720cc69d1627d05d494689a15cc64
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-20 13:41:01 -0500

    DashboardController

--- src/main/java/com/app/controller/DashboardController.java
+++ src/main/java/com/app/controller/DashboardController.java
@@ -1,17 +1,14 @@
 package com.app.controller;
 
 import com.app.service.DashboardService;
-import com.app.service.InvoiceProductService;
 import com.app.service.InvoiceService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/dashboard")
 @AllArgsConstructor
 public class DashboardController {
 
@@ -19,11 +16,11 @@ public class DashboardController {
     private final InvoiceService invoiceService;
 
 
-
-    @GetMapping(value = {})
+    @GetMapping("/dashboard")
     public String dashboard(Model model) {
         model.addAttribute("summaryNumbers", dashboardService.getSummaryNumbers());
         model.addAttribute("invoices", invoiceService.listLast3ApprovedInvoices());
+        model.addAttribute("exchangeRates", dashboardService.getCachedCurrencyDto());
         return "dashboard";
     }
 
