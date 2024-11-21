8d708162768891e7725755ef08c9a35a6607e118
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-21 11:38:18 -0500

    InvoiceProductServiceImpl

--- src/main/java/com/app/service/InvoiceProductService.java
+++ src/main/java/com/app/service/InvoiceProductService.java
@@ -1,27 +1,19 @@
 package com.app.service;
 
 import com.app.dto.InvoiceProductDto;
-import com.app.entity.InvoiceProduct;
-import com.app.enums.ClientVendorType;
-import com.app.enums.InvoiceType;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface InvoiceProductService {
 
-
-    InvoiceProductDto findInvoiceProductById(Long id);
-    InvoiceProductDto findInvoiceProductByInvoiceId(Long id);
-
-    List<InvoiceProductDto> listAllInvoiceProductsByInvoiceTypeAndLoggedInUserOrderByInvoiceNoDesc(InvoiceType invoiceType);
-
-    List<InvoiceProductDto> listAllInvoiceProductsByInvoiceTypePurchase();
-
-    List<InvoiceProductDto> listAllInvoiceByInvoiceTypeOrderByInvoiceNoDes(InvoiceType invoiceType);
-
-//    @Query("select ip from InvoiceProduct ip join Invoice i on ip.invoice.id = i.id where i.invoiceType='Purchase'")
-//    List<InvoiceProductDto> findAllProductsByInvoiceTypePurchase();
-
+    InvoiceProductDto listInvoiceProductById(Long id);
+    List<InvoiceProductDto> listAllByInvoiceIdAndCalculateTotalPrice(Long id);
+    List<InvoiceProductDto> listInvoiceProductByInvoiceId(Long invoiceId);
+    List<InvoiceProductDto> listAllApprovedInvoiceProductsOfCompany();
+    void saveInvoiceProduct(Long invoiceId, InvoiceProductDto dto);
+    void deleteInvoiceProduct(Long invoiceProductId);
+    void updateRemainingQuantityUponPurchaseApproval(Long id);
+    void updateQuantityInStockPurchase(Long id);
+    void updateQuantityInStockSale(Long id);
+    void calculateProfitOrLoss(Long id);
 }
