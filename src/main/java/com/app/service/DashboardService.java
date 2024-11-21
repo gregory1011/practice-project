6b18a1bb0634cf0633d8f9b0fab1c1b77e87235e
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-21 11:37:45 -0500

    DashboardService

--- src/main/java/com/app/service/DashboardService.java
+++ src/main/java/com/app/service/DashboardService.java
@@ -1,5 +1,12 @@
 package com.app.service;
 
+import com.app.dto.common.CurrencyDto;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
 public interface DashboardService {
 
+    CurrencyDto getCachedCurrencyDto();
+    Map<String, BigDecimal> getSummaryNumbers();
 }
