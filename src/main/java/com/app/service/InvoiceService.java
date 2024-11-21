a4afd88a21a63f4dc867fe1e400bccf19b115097
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-21 11:38:50 -0500

    InvoiceService

--- src/main/java/com/app/service/InvoiceService.java
+++ src/main/java/com/app/service/InvoiceService.java
@@ -3,14 +3,23 @@ package com.app.service;
 import com.app.dto.InvoiceDto;
 import com.app.enums.InvoiceType;
 
+import java.math.BigDecimal;
 import java.util.List;
+
 
 public interface InvoiceService {
 
-    InvoiceDto findInvoiceById(Long id);
-    List<InvoiceDto> listAllInvoiceByInvoiceTypeOrderByInvoiceNo(InvoiceType invoiceType);
-    String getLastInvoiceId(InvoiceType invoiceType);
-    void saveSaleInvoice(InvoiceDto invoiceDto);
-    void savePurchaseInvoice(InvoiceDto invoiceDto);
+    InvoiceDto listInvoiceById(Long id);
+    List<InvoiceDto> listAllByInvoiceType(InvoiceType invoiceType);
     List<InvoiceDto> listAllInvoiceByClientVendorId(Long id);
+    InvoiceDto generateNewInvoiceDto(InvoiceType invoiceType);
+    InvoiceDto saveInvoice(InvoiceDto invoiceDto, InvoiceType invoiceType);
+    void approveInvoice(Long id);
+    void updateInvoice(InvoiceDto dto);
+    void deleteInvoice(Long id);
+    List<InvoiceDto> listLast3ApprovedInvoices();
+    BigDecimal sumTotal(InvoiceType invoiceType);
+    BigDecimal sumProfitLoss();
+    InvoiceDto printInvoiceId(Long id);
 }
+
