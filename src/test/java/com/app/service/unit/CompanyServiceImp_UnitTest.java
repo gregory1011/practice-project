5f792f40eed36f0b996798e78bfe7dca2bf70ab1
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-27 18:06:33 -0500

    CompanyServiceImp_UnitTest

---
+++ src/test/java/com/app/service/unit/CompanyServiceImp_UnitTest.java
@@ -0,0 +1,88 @@
+package com.app.service.unit;
+
+
+import com.app.dto.CompanyDto;
+import com.app.dto.UserDto;
+import com.app.entity.Company;
+import com.app.entity.Role;
+import com.app.entity.User;
+import com.app.enums.CompanyStatus;
+import com.app.repository.CompanyRepository;
+import com.app.service.SecurityService;
+import com.app.service.TestDocInitializer;
+import com.app.service.impl.CompanyServiceImpl;
+import com.app.util.MapperUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class CompanyServiceImp_UnitTest {
+
+    @Mock
+    private CompanyRepository companyRepository;
+    @Spy
+    private MapperUtil mapperUtil= new MapperUtil(new ModelMapper());
+    @Mock
+    private SecurityService securityService;
+    @InjectMocks
+    private CompanyServiceImpl companyService;
+
+    private Company company;
+    private CompanyDto companyDto;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        company= new Company();
+        company.setId(1L);
+        company.setTitle("Test_Company");
+        company.setCompanyStatus(CompanyStatus.ACTIVE);
+
+        companyDto= new CompanyDto();
+        companyDto.setId(1L);
+        companyDto.setTitle("Test_Company");
+        companyDto.setCompanyStatus(CompanyStatus.ACTIVE);
+
+        Role role= new Role();
+        role.setId(1L);
+        role.setDescription("Admin");
+        user= new User();
+        user.setRole(role);
+        user.setCompany(company);
+
+    }
+
+    @Test
+    void getCompanyByLoggedInUser_shouldReturnCompanyDto(){
+        //given
+        CompanyDto expectedResult = TestDocInitializer.getCompany(CompanyStatus.ACTIVE);
+        UserDto loggedInUser = TestDocInitializer.getUser("Admin");
+
+        //when
+        when(securityService.getLoggedInUser()).thenReturn(loggedInUser);
+
+        CompanyDto actualResult = companyService.getCompanyByLoggedInUser();
+
+        assertThat(actualResult).usingRecursiveComparison().isEqualTo(expectedResult);
+        verify(securityService, times(1)).getLoggedInUser();
+    }
+
+
+}
+
+
