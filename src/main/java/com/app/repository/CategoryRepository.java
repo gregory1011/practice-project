6ea61780e82a5b3d951033cdf6390085eb76eab1
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-26 20:40:30 -0500

    CategoryRepository

--- src/main/java/com/app/repository/CategoryRepository.java
+++ src/main/java/com/app/repository/CategoryRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    List<Category> findByCompanyId(Long companyId);
     @Query(value = "select c from categories c where c.description= ?1 and c.company.id= ?2")
     Category findCategoryByDescriptionAndCompanyId (String description, Long companyId);
 
