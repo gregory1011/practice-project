89178a1ca6a56522ad34a9585f9ae2f5565192dc
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-28 09:42:32 -0500

    CategoryServiceImp_IntTest

--- src/test/java/com/app/service/integration/CategoryServiceImp_IntTest.java
+++ src/test/java/com/app/service/integration/CategoryServiceImp_IntTest.java
@@ -1,14 +1,23 @@
 package com.app.service.integration;
 
 
+import com.app.dto.CategoryDto;
+import com.app.exceptions.CategoryNotFoundException;
+import com.app.exceptions.UserNotFoundException;
 import com.app.repository.CategoryRepository;
 import com.app.service.CategoryService;
-import com.app.service.SecurityService;
 import com.app.service.SecuritySetUpUtil;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CategoryServiceImp_IntTest {
@@ -23,6 +32,29 @@ public class CategoryServiceImp_IntTest {
     @BeforeEach
     void setUp() {
         SecuritySetUpUtil.setUpSecurityContext();
+    }
+
+
+    @Test
+    void testFindById() {
+        Long id = 1L;
+        CategoryDto categoryDto = categoryService.findById(id);
+        assertThat(categoryDto).isNotNull();
+        assertThat(categoryDto.getId()).isEqualTo(id);
+    }
+
+    @Test
+    void testFindById_NotFound() {
+        Throwable throwable = catchThrowable(() -> categoryService.findById(-1L));
+        assertThat(throwable).isInstanceOf(CategoryNotFoundException.class);
+        assertThat(throwable.getMessage()).isEqualTo("Category not found");
+    }
+
+    @Test
+    void testListAllCategories() {
+        List<CategoryDto> dtoList = categoryService.listAllCategories();
+        assertThat(dtoList).isNotEmpty();
+        assertThat(dtoList).isNotNull();
     }
 
 
