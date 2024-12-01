05f0ea2a9bd4036437b902dd522e5c33c5891001
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-11-30 22:32:31 -0500

    CategoryServiceImpl_IntTest

--- src/test/java/com/app/service/integration/CategoryServiceImp_IntTest.java
+++ src/test/java/com/app/service/integration/CategoryServiceImp_IntTest.java
@@ -2,8 +2,8 @@ package com.app.service.integration;
 
 
 import com.app.dto.CategoryDto;
+import com.app.entity.Category;
 import com.app.exceptions.CategoryNotFoundException;
-import com.app.exceptions.UserNotFoundException;
 import com.app.repository.CategoryRepository;
 import com.app.service.CategoryService;
 import com.app.service.SecuritySetUpUtil;
@@ -12,13 +12,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.ThrowableAssert.catchThrowable;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 public class CategoryServiceImp_IntTest {
@@ -76,4 +76,27 @@ public class CategoryServiceImp_IntTest {
         assertThat(updatedCategory.getId()).isNotNull();
         assertThat(updatedCategory.getDescription()).isEqualTo(categoryDto.getDescription());
     }
+
+    @Test
+    @Transactional // we use transactional to handle deleteCategory() -> when the category: isDeleted= true, and we search for id (in entity class @Where(clause = "is_deleted=false")
+    void testDeleteById() {
+        Long id = 1L;
+        categoryService.deleteById(id);
+        Category category = categoryRepository.findById(id).orElse(null);
+        assertThat(category).isNotNull();
+        assertThat(category.getId()).isEqualTo(id);
+        assertThat(category.getDescription()).isNotNull();
+        assertThat(category.getIsDeleted()).isTrue();
+
+        //negative scenario id= -1; throws exception
+        Throwable throwable = catchThrowable(() -> categoryService.findById(-1L));
+        assertThat(throwable).isInstanceOf(CategoryNotFoundException.class);
+        assertThat(throwable.getMessage()).isEqualTo("Category not found");
+    }
+
+    @Test
+    void testIsDescriptionExists() {
+        categoryRepository.findCategoryByDescriptionAndCompanyId("Test", 1L);
+    }
+
 }
