73a9d6c4ad5bd5ad2a65ccfddee97d5a45258f99
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-02 13:08:25 -0500

    CompanyRepository

--- src/main/java/com/app/repository/CompanyRepository.java
+++ src/main/java/com/app/repository/CompanyRepository.java
@@ -4,9 +4,11 @@ import com.app.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-    Company findByTitle(String name);
+    Optional<Company> findByTitle(String name);
 }
