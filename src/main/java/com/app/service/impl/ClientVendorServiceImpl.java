f04aefbdac2246999b626994a93d7d937fcc9268
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-06 11:16:44 -0500

    ClientVendorServiceImpl

--- src/main/java/com/app/service/impl/ClientVendorServiceImpl.java
+++ src/main/java/com/app/service/impl/ClientVendorServiceImpl.java
@@ -7,10 +7,12 @@ import com.app.enums.ClientVendorType;
 import com.app.exceptions.ClientVendorNotFoundException;
 import com.app.repository.ClientVendorRepository;
 import com.app.service.ClientVendorService;
+import com.app.service.CompanyService;
 import com.app.service.SecurityService;
 import com.app.util.MapperUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
@@ -23,59 +25,72 @@ public class ClientVendorServiceImpl implements ClientVendorService {
 
     private final ClientVendorRepository clientVendorRepository;
     private final MapperUtil mapperUtil;
-    private final SecurityService securityService;
+    private final CompanyService companyService;
+
 
     @Override
-    public List<ClientVendorDto> listAllClientVendors() {
-        Long id = securityService.getLoggedInUser().getCompany().getId();
-        return clientVendorRepository.findAll().stream()
-                .filter(m->m.getCompany().getId().equals(id) && !m.getIsDeleted())
-                .map(each -> mapperUtil.convert(each,new ClientVendorDto()))
-                .sorted(Comparator.comparing(ClientVendorDto::getClientVendorType).reversed())
-                .toList();
-    }
-
-    @Override
-    public List<ClientVendorDto> listAllByClientVendorTypeAndCompanyId(ClientVendorType clientVendorType) {
-        Long companyId = securityService.getLoggedInUser().getCompany().getId();
-        return clientVendorRepository.findAll().stream()
-                .filter(m->m.getCompany().getId().equals(companyId))
-                .filter(m->m.getClientVendorType().equals(clientVendorType))
-                .sorted(Comparator.comparing(ClientVendor::getClientVendorName))
-                .map(each->mapperUtil.convert(each, new ClientVendorDto())).toList();
-    }
-
-    @Override
-    public void saveClientVendor(ClientVendorDto clientVendorDto) {
-        CompanyDto company = securityService.getLoggedInUser().getCompany();
-        clientVendorDto.setCompany(company);
-        ClientVendor obj = mapperUtil.convert(clientVendorDto, new ClientVendor());
-        clientVendorRepository.save(obj);
-    }
-
-    @Override
-    public void deleteClientVendor(Long id) {
-        Optional<ClientVendor> clientVendor= clientVendorRepository.findById(id);
-        clientVendor.ifPresent(client -> client.setIsDeleted(true));
-        clientVendorRepository.save(clientVendor.orElseThrow(ClientVendorNotFoundException::new));
-    }
-
-    @Override
-    public ClientVendorDto listClientVendorById(Long id) {
+    @Transactional(readOnly = true) //
+    public ClientVendorDto findById(Long id) {
         ClientVendor clientVendor = clientVendorRepository.findById(id).orElseThrow(ClientVendorNotFoundException::new);
         return mapperUtil.convert(clientVendor,new ClientVendorDto());
     }
 
     @Override
-    public void updateClientVendor(Long id, ClientVendorDto dto) {
+    public List<ClientVendorDto> listAllClientVendors() {
+        Long id = companyService.getCompanyByLoggedInUser().getId();
+        return clientVendorRepository.findByCompanyId(id).stream()
+                .map(each ->{
+                    ClientVendorDto dto= mapperUtil.convert(each, new ClientVendorDto());
+                    dto.setHasInvoice(!each.getInvoices().isEmpty());
+                    return dto;
+                })
+                .sorted(Comparator.comparing(ClientVendorDto::getClientVendorType).reversed()
+                        .thenComparing(ClientVendorDto::getClientVendorName))
+                .toList();
+    }
+
+    @Override
+    public List<ClientVendorDto> listAllByClientVendorType(ClientVendorType clientVendorType) {
+        Long id = companyService.getCompanyByLoggedInUser().getId();
+        return clientVendorRepository.findAll().stream()
+                .filter(m->m.getClientVendorType().equals(clientVendorType))
+                .filter(m->m.getCompany().getId().equals(id))
+                .map(each->mapperUtil.convert(each, new ClientVendorDto()))
+                .toList();
+    }
+
+    @Override
+    public ClientVendorDto saveClientVendor(ClientVendorDto clientVendorDto) {
+        clientVendorDto.setCompany(companyService.getCompanyByLoggedInUser());
+        ClientVendor saved = clientVendorRepository.save(mapperUtil.convert(clientVendorDto, new ClientVendor()));
+        return mapperUtil.convert(saved, new ClientVendorDto());
+    }
+
+    @Override
+    public ClientVendorDto updateClientVendor(ClientVendorDto dto) {
+        ClientVendor clientVendor = clientVendorRepository.findById(dto.getId()).orElseThrow(ClientVendorNotFoundException::new);
+
+        dto.getAddress().setId(clientVendor.getAddress().getId());
+        dto.setCompany(companyService.getCompanyByLoggedInUser());
+
+        ClientVendor saved = clientVendorRepository.save(mapperUtil.convert(dto, new ClientVendor()));
+        return mapperUtil.convert(saved, new ClientVendorDto());
+    }
+
+    @Override
+    public void deleteClientVendor(Long id) {
         ClientVendor clientVendor = clientVendorRepository.findById(id).orElseThrow(ClientVendorNotFoundException::new);
-        ClientVendor convert = mapperUtil.convert(dto, new ClientVendor());
-        clientVendor.setAddress(convert.getAddress());
-        clientVendor.setClientVendorType(dto.getClientVendorType());
-        clientVendor.setClientVendorName(dto.getClientVendorName());
-        clientVendor.setPhone(dto.getPhone());
-        clientVendor.setWebsite(dto.getWebsite());
+        clientVendor.setIsDeleted(true);
+        clientVendor.getAddress().setIsDeleted(true);
+        clientVendor.setClientVendorName(clientVendor.getClientVendorName()+ "-"+id);
         clientVendorRepository.save(clientVendor);
+    }
+
+    @Override
+    public boolean isClientVendorNameExists(String clientVendorName) {
+        Long companyId = companyService.getCompanyByLoggedInUser().getId();
+        Optional<ClientVendor> clientVendor = clientVendorRepository.existsByCompanyIdAndClientVendorName(companyId, clientVendorName);
+        return clientVendor.isPresent(); // if exists return true, else false
     }
 
 }
