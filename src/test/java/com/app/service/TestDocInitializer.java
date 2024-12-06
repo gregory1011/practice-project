5e7df9f54296afd7fea978be595555b0787d6777
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-06 11:17:11 -0500

    TestDocInitializer

--- src/test/java/com/app/service/TestDocInitializer.java
+++ src/test/java/com/app/service/TestDocInitializer.java
@@ -40,5 +40,38 @@ public class TestDocInitializer {
                 .build();
     }
 
+    public static ClientVendorDto getClientVendorDto(ClientVendorType clientVendorType){
+        ClientVendorDto clientVendorDto = new ClientVendorDto();
+        clientVendorDto.setId(1L);
+        clientVendorDto.setCompany(getCompany(CompanyStatus.ACTIVE));
+        clientVendorDto.setPhone("123-456-1233");
+        clientVendorDto.setWebsite("www.test.net");
+        clientVendorDto.setClientVendorType(clientVendorType);
+        clientVendorDto.setAddress(getAddress());
+        clientVendorDto.setCompany(getCompany(CompanyStatus.ACTIVE));
+        return clientVendorDto;
+    }
+
+    public static AddressDto getAddress(){
+        AddressDto addressDto = new AddressDto();
+        addressDto.setId(1L);
+        addressDto.setCountry("US");
+        addressDto.setAddressLine1("120 West Ave");
+        addressDto.setCity("New York");
+        addressDto.setState("NY");
+        addressDto.setAddressLine2("Suite 10");
+        addressDto.setZipCode("90904");
+        return addressDto;
+    }
+
+    public static InvoiceDto getInvoiceDto(InvoiceStatus status, InvoiceType invoiceType){
+        InvoiceDto invoiceDto = new InvoiceDto();
+        invoiceDto.setId(1L);
+        invoiceDto.setCompany(getCompany(CompanyStatus.ACTIVE));
+        invoiceDto.setInvoiceNo("P-123");
+        invoiceDto.setInvoiceStatus(status);
+        invoiceDto.setInvoiceType(invoiceType);
+        return invoiceDto;
+    }
 
 }
