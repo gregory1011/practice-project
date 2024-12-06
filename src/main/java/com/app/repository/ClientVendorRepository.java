13ee03646b5fb86d7b8e90866c43a9dd2ba2799f
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-06 11:16:19 -0500

    ClientVendorRepository

--- src/main/java/com/app/repository/ClientVendorRepository.java
+++ src/main/java/com/app/repository/ClientVendorRepository.java
@@ -4,8 +4,13 @@ import com.app.entity.ClientVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface ClientVendorRepository extends JpaRepository<ClientVendor, Long> {
 
+    List<ClientVendor> findByCompanyId(Long companyId);
+    Optional<ClientVendor> existsByCompanyIdAndClientVendorName(Long companyId, String clientVendorName);
 }
