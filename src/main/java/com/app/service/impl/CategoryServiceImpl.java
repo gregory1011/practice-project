fd7012cd664372749ba19f6483058c6cce0b3e5a
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-07 11:00:47 -0500

    CategoryServiceImpl

--- src/main/java/com/app/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/app/service/impl/CategoryServiceImpl.java
@@ -9,6 +9,7 @@ import com.app.service.CategoryService;
 import com.app.service.SecurityService;
 import com.app.util.MapperUtil;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -16,7 +17,7 @@ import java.util.List;
 
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
