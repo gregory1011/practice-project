9fb482594ded0c0a5c98754a8a1e5686b6e6d920
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-10 10:35:49 -0500

    RoleDto

--- src/main/java/com/app/dto/RoleDto.java
+++ src/main/java/com/app/dto/RoleDto.java
@@ -1,10 +1,10 @@
 package com.app.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
-@ToString
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoleDto {
