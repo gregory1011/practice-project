43be566c0b97e4a2c7afc0534d7d42eb9c8da8fa
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-10 10:34:59 -0500

    TokenDto

--- src/main/java/com/app/dto/common/TokenDto.java
+++ src/main/java/com/app/dto/common/TokenDto.java
@@ -4,12 +4,10 @@ package com.app.dto.common;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 
-@Getter
-@Setter
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TokenDto {
