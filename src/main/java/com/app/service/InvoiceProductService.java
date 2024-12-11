cd98f0b5f85ec3f0e014a991030341aacce0b72f
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-11 14:48:28 -0500

    InvoiceProductService

--- src/main/java/com/app/service/InvoiceProductService.java
+++ src/main/java/com/app/service/InvoiceProductService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface InvoiceProductService {
 
-    InvoiceProductDto listInvoiceProductById(Long id);
+    InvoiceProductDto findById(Long id);
     List<InvoiceProductDto> listAllByInvoiceIdAndCalculateTotalPrice(Long id);
     List<InvoiceProductDto> listInvoiceProductByInvoiceId(Long invoiceId);
     List<InvoiceProductDto> listAllApprovedInvoiceProductsOfCompany();
