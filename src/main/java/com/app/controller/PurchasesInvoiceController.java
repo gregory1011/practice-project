e318e578e6c498e73e4c3af16af85069e465e5ab
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-23 22:16:12 -0500

    PurchasesInvoiceController

--- src/main/java/com/app/controller/PurchasesInvoiceController.java
+++ src/main/java/com/app/controller/PurchasesInvoiceController.java
@@ -76,7 +76,7 @@ public class PurchasesInvoiceController {
             model.addAttribute("invoiceProducts", invoiceProductService.listAllByInvoiceIdAndCalculateTotalPrice(invoiceId));
             return "invoice/purchase-invoice-update";
         }
-        invoiceProductService.saveInvoiceProduct(invoiceId, dto);
+        invoiceProductService.save(invoiceId, dto);
         return "redirect:/purchaseInvoices/update/"+invoiceId;
     }
 
