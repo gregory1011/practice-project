317405618dded05ad1ac43d04c8ae24748b7b20d
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2024-12-27 11:49:43 -0500

    ProductRepository

--- src/main/java/com/app/repository/ProductRepository.java
+++ src/main/java/com/app/repository/ProductRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p from products  p where p.category.company.id= ?1")
+    List<Product> findByCompanyId(Long companyId);
 
     @Query(value = "SELECT p FROM products p WHERE p.name= ?1 AND p.category.company.id= ?2")
     Product findProductByNameAndCompanyId(String name, Long companyId);
