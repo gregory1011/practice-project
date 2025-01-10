bec54aff165a6f64f9a9dc81906c4467cf238f47
Gregory Rosca <123904350+gregory1011@users.noreply.github.com>
2025-01-10 15:42:43 -0500

    ReportServiceImpl_UnitTest

---
+++ src/test/java/com/app/service/unit/ReportServiceImpl_UnitTest.java
@@ -0,0 +1,21 @@
+package com.app.service.unit;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+
+
+@ExtendWith(MockitoExtension.class)
+public class ReportServiceImpl_UnitTest {
+
+
+
+}
